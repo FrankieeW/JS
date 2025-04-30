bedea5df7b4d2280fc9c16a6c55326d54c9d4df2
Frankie <frankie.fc.wang@outlook.com>
2025-05-01 03:23:35 +0800

    Update version to 0.4 and fix ad module script path to use HTTPS

--- TM/test.js
+++ TM/test.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         引用广告模块示例
 // @namespace    http://tampermonkey.net/
-// @version      0.3@01-05-2025
+// @version      0.4@01-05-2025
 // @description  引用并使用广告模块
 // @author       Frankie
 // @match        *://*/*
@@ -15,7 +15,7 @@
 
     // 加载广告模块
     const adScript = document.createElement('script');
-    adScript.src = 'blog.frankie.science/TM/adModule.js'; // 这里替换成你广告模块的实际路径
+    adScript.src = 'https://blog.frankie.science/TM/adModule.js'; // 这里替换成你广告模块的实际路径
     document.head.appendChild(adScript);
 
     // 确保广告模块加载完成后再调用
